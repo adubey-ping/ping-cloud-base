64bc03a8e9f55ba8f41242d4c4b006e5470c4889
Abhishek Dubey <adubey@pingidentity.com>
2025-02-07 11:23:05 +0530

    Add check

--- code-gen/generate-cluster-state.sh
+++ code-gen/generate-cluster-state.sh
@@ -1538,6 +1538,11 @@ for ENV_OR_BRANCH in ${SUPPORTED_ENVIRONMENT_TYPES}; do
 
   echo "=====> Done creating environment '${ENV}'"
 )
+subshell_exit_code=$?
+if [[ $subshell_exit_code -ne 0 ]]; then
+    echo "Subshell failed with exit code $subshell_exit_code"
+    exit $subshell_exit_code
+fi
 done
 
 cp -p push-cluster-state.sh "${TARGET_DIR}"
